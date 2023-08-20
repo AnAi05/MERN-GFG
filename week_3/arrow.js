1a123bef4b46a712bc2310f3ba97b2ce22fbde17
ANAi <omkarjajulwar@gmail.com>
2023-08-20 22:57:13 +0530

    week_3

---
+++ week_3/arrow.js
@@ -0,0 +1,14 @@
+
+
+// ES6 features
+const sum = (a, b) => a + b;
+
+const result = sum(2, 3);
+
+console.log(result);
+
+function isPositive(number) {
+  return number >= 0;
+}
+
+const isPositive = (number) => number >= 0;
